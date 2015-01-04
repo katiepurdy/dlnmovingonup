31edbd0bb5e67dc03790489323a09a22160d1e43
Dayne May <daynemay@gmail.com>
2015-01-04 18:25:41 -0400

    Use course.html for all courses

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 TODO:
-- Multi-level menus: e.g. Mathematics > Fractions > Improper Fractions
 - CSS for hover of nav menu
 - write up how to add a new course
 - Change active setting on each page
 - iframe body to be #eee
+- make course.html load the nominated course (nominated by URL GET parameter)
+- delete fractions.html 
 
-
-- Use about.html as a basis for other .html files (CDN usage, etc)
+- Use the old about.html as a basis for other .html files ( in terms of CDN usage, etc)
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 // Load header and footer
 $(function(){ $("#dln-header").load("header.html") });
 $(function(){ $("#dln-footer").load("footer.html") });
+
+function loadCourse() {
+	var params = getSearchParameters();
+	var course = "/courses/" + params.course + "/story.html";
+	$(".dln-course-iframe").attr('src', course);
+}
+
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssociativeArray(prmstr) : {};
+}
+
+function transformToAssociativeArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
